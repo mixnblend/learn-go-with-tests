395ff353312e55902e45a3a02d29aa00419a0644
Jonathan Arnold <jon@mixnblend.com>
2024-05-05 17:30:48 +0200

    feat(arrays): add sum all slices function and test

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -1,14 +1,29 @@
 package main
 
-import "testing"
+import (
+	"slices"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 4, 5}
 
-	got := Sum(numbers)
-	want := 15
+	t.Run("collection of 5 numbers", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5}
 
-	if got != want {
-		t.Errorf("got %d want %d given, %v", got, want, numbers)
+		got := Sum(numbers)
+		want := 15
+
+		if got != want {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	})
+}
+
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
+
+	if !slices.Equal(got, want) {
+		t.Errorf("got %v, want %v", got, want)
 	}
 }
