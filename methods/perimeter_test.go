f71f59fda4c4d34900caf0ae818a64d3c61a69fd
Jonathan Arnold <jon@mixnblend.com>
2024-05-05 20:51:16 +0200

    refactor(methods): add rectangle struct

--- methods/perimeter_test.go
+++ methods/perimeter_test.go
@@ -3,19 +3,21 @@ package methods
 import "testing"
 
 func TestPerimeter(t *testing.T) {
-	got := Perimeter(10.0, 10.0)
+	rectangle := Rectangle{10.0, 10.0}
+	got := Perimeter(rectangle)
 	want := 40.0
 
 	if got != want {
-		t.Errorf("wanted %.2f, got %.2f", got, want)
+		t.Errorf("wanted %.2f, got %.2f", want, got)
 	}
 }
 
 func TestArea(t *testing.T) {
-	got := Area(12.0, 6.0)
+	rectangle := Rectangle{12.0, 6.00}
+	got := Area(rectangle)
 	want := 72.00
 
 	if got != want {
-		t.Errorf("wanted %.2f, got %.2f", got, want)
+		t.Errorf("wanted %.2f, got %.2f", want, got)
 	}
 }
