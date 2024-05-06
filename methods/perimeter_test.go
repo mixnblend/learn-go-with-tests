91d9b058af83b9aafae171d299f8110afa0ba5d4
Jonathan Arnold <jon@mixnblend.com>
2024-05-06 09:10:18 +0200

    refactor(methods): clean up tests to use name

--- methods/perimeter_test.go
+++ methods/perimeter_test.go
@@ -14,19 +14,22 @@ func TestPerimeter(t *testing.T) {
 
 func TestArea(t *testing.T) {
 	areaTests := []struct {
-		shape Shape
-		want  float64
+		name    string
+		shape   Shape
+		hasArea float64
 	}{
-		{Rectangle{Width: 12.0, Height: 6.00}, 72.0},
-		{Circle{Radius: 10}, 314.1592653589793},
-		{Triangle{Base: 12, Height: 6}, 36.0},
+		{name: "Rectangle", shape: Rectangle{Width: 12.0, Height: 6.00}, hasArea: 72.0},
+		{name: "Circle", shape: Circle{Radius: 10}, hasArea: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{Base: 12, Height: 6}, hasArea: 36.0},
 	}
 
 	for _, tt := range areaTests {
-		got := tt.shape.Area()
-		want := tt.want
-		if got != want {
-			t.Errorf("wanted %g, got %g", want, got)
-		}
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			want := tt.hasArea
+			if got != want {
+				t.Errorf("wanted %g, got %g", want, got)
+			}
+		})
 	}
 }
