04907e860ea4fad3df46dde3bb9859e6aab4f495
Jonathan Arnold <jon@mixnblend.com>
2024-05-07 09:03:50 +0200

    feat(wallet): add check for over withdrawals and add test

--- wallet/wallet_test.go
+++ wallet/wallet_test.go
@@ -5,34 +5,46 @@ import (
 )
 
 func TestWallet(t *testing.T) {
-
 	assertBalance := func(t testing.TB, wallet Wallet, want Bitcoin) {
-		got := wallet.Balance()
-
 		t.Helper()
+		got := wallet.Balance()
 		if got != want {
 			t.Errorf("got %s want %s", got, want)
 		}
 	}
 
-	t.Run("Deposit", func(t *testing.T) {
+	assertError := func(t testing.TB, err error) {
+		t.Helper()
+		if err == nil {
+			t.Error("wanted an error but didn't get one")
+		}
+	}
+
+	t.Run("deposit", func(t *testing.T) {
 		wallet := Wallet{}
 
 		wallet.Deposit(Bitcoin(10))
 
 		want := Bitcoin(10)
 		assertBalance(t, wallet, want)
-
 	})
 
-	t.Run("Withdraw", func(t *testing.T) {
+	t.Run("withdraw", func(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(20)}
 
 		wallet.Withdraw(Bitcoin(10))
 
 		want := Bitcoin(10)
-
 		assertBalance(t, wallet, want)
 	})
 
+	t.Run("withdraw insufficient funds", func(t *testing.T) {
+		startingBalance := Bitcoin(20)
+		wallet := Wallet{startingBalance}
+
+		err := wallet.Withdraw(Bitcoin(100))
+
+		assertBalance(t, wallet, startingBalance)
+		assertError(t, err)
+	})
 }
