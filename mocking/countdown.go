c091fe23e66d0ed25b4251ec8803a16fa70dd0f2
Jonathan Arnold <jon@mixnblend.com>
2024-07-07 21:35:52 +0200

    feat(mocking): add sleeper function and mocks with tests

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -11,6 +11,15 @@ type Sleeper interface {
 	Sleep()
 }
 
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep    func(time.Duration)
+}
+
+func (c *ConfigurableSleeper) Sleep() {
+	c.sleep(c.duration)
+}
+
 type DefaultSleeper struct{}
 
 func (d *DefaultSleeper) Sleep() {
@@ -31,6 +40,6 @@ func Countdown(out io.Writer, sleeper Sleeper) {
 }
 
 func main() {
-	sleeper := &DefaultSleeper{}
+	sleeper := &ConfigurableSleeper{1 * time.Second, time.Sleep}
 	Countdown(os.Stdout, sleeper)
 }
